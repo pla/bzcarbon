4a0c0f5c121b30299717ed6809359ca0617f3ad8
pla <massenpost@pla1.de>
2025-06-16 11:57:23 +0200

    minor fix

--- carbon-recipe.lua
+++ carbon-recipe.lua
@@ -306,7 +306,7 @@ data:extend({
       {type="fluid", name=ammonia, amount=5},
       {type="fluid", name="water", amount=10},
       {type="fluid", name="sulfuric-acid", amount=5},
-    }  or util.k2() {
+    }  or util.k2() and {
       {type="item", name="graphite", amount=1},
       {type="fluid", name=ammonia, amount=10},
       {type="fluid", name="sulfuric-acid", amount=10},
